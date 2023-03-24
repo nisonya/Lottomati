d6fc79b7d4d096e38f052b1e444881202962baf5
USER <nikison1804@mail.ru>
2023-03-24 17:33:53 +0300

    added No Internet activity

---
+++ app/src/main/java/com/example/test_nikitina/InernetNone.java
@@ -0,0 +1,14 @@
+package com.example.test_nikitina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InernetNone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inernet_none);
+    }
+}
