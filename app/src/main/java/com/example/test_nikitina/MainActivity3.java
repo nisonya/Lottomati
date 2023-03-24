1c446ec76ac7d5fe6f3a4c3a7f31631a6b90fc0c
USER <nikison1804@mail.ru>
2023-03-24 12:47:44 +0300

    unable reload on changing orientation

--- app/src/main/java/com/example/test_nikitina/MainActivity3.java
+++ app/src/main/java/com/example/test_nikitina/MainActivity3.java
@@ -2,8 +2,12 @@ package com.example.test_nikitina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -12,7 +16,23 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("https://developer.android.com/reference/android/arch/persistence/room/package-summary");
+        webView.loadUrl("https://www.youtube.com/watch?v=iABVUYsk-Jw&t=3s&ab_channel=%D0%AE%D0%BB%D0%B8%D0%BA");
+        //корректность данных
+        webView.getSettings().setJavaScriptEnabled(true);
+        //loadUrl только в приложение без стороних программ
+        WebViewClient webViewClient = new WebViewClient() {
+            @SuppressWarnings("deprecation") @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
 
+            @TargetApi(Build.VERSION_CODES.N) @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        };
+        webView.setWebViewClient(webViewClient);
     }
 }
